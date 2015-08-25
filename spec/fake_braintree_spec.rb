5b65e978e90b3b8c62dfdc4cb79ee7a42e2873fa
Scott Rocher <scottrocher@gmail.com>
2015-08-24 17:16:50 -0700

    Update VALID_CREDIT_CARDS to match latest braintree_ruby gem values
    Sets spec expectations to use the gem array to detect any future
    changes.

--- spec/fake_braintree_spec.rb
+++ spec/fake_braintree_spec.rb
@@ -98,15 +98,7 @@ end
 
 describe FakeBraintree, 'VALID_CREDIT_CARDS' do
   it 'includes only credit cards that are valid in the Braintree sandbox' do
-    valid_credit_cards = %w(
-      4111111111111111 4005519200000004
-      4009348888881881 4012000033330026
-      4012000077777777 4012888888881881
-      4217651111111119 4500600000000061
-      5555555555554444 378282246310005
-      371449635398431 6011111111111117
-      3530111333300000
-    )
+    valid_credit_cards = ::Braintree::Test::CreditCardNumbers::All
 
     expect(FakeBraintree::VALID_CREDIT_CARDS.sort).to eq valid_credit_cards.sort
   end
